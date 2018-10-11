729118ce412a092b0d58f37f942039bc05cc9591
Abiud <abiudcantu@gmail.com>
2018-10-10 21:58:42 -0500

    added angularfire modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyD9JCVyaFwcgm7etjC9D_-ZZ24VbRx7ff4',
+    authDomain: 'ionic4-fire-f8a53.firebaseapp.com',
+    databaseURL: 'https://ionic4-fire-f8a53.firebaseio.com',
+    projectId: 'ionic4-fire-f8a53',
+    storageBucket: 'ionic4-fire-f8a53.appspot.com',
+    messagingSenderId: '1023299584621'
+  }
 };
 
 /*
